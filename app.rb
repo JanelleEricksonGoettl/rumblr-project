a24eb70da671a888910469251fe32695b4e264ba
Janelle Erickson-Goettl <janelle.erickson.goettl@gmail.com>
2019-01-23 13:08:08 -0500

    debugging routes. Commented out all routes not tested, for clarity. Sign_up, dashboad, and landing/index function.

--- app.rb
+++ app.rb
@@ -10,22 +10,64 @@ get "/" do
 end
 
 #dn display user's last 20 posts based on user_id
-get "/users/:id" do
-  @user = User.find(params["id"])
-  #dn set the erb file once created
-    # figure out how to set to display last 20 posts
-end
+# get "/users/:id" do
+#   @user = User.find(params["id"])
+#   #dn set the erb file once created
+#     # figure out how to set to display last 20 posts
+# end
 
-get "/posts/:id" do
-  @post = Post.find(params["id"])
-  #dn set @posts = Post.all later
-end
+# get "/posts/:id" do
+#   @post = Post.find(params["id"])
+#   #dn set @posts = Post.all later
+# end
 
 ### User routes ###
 
+#dn dashboard
+get "/dashboard" do
+  if session[:user_id] != nil
+  erb :dashboard
+  else
+    # flash[:warning] = 'Oops!'
+    redirect '/'
+  end
+end
+
+# get "/profile" do
+#   if session[:user_id] == true
+#     erb :index
+#   else
+#     # flash[:warning] = 'Oops!'
+#     redirect '/' #signup'
+#   end
+# end
+
+### Post routes ###
+
+# get "/new_post" do
+#   if session[:user_id] == true
+#     erb :new_post
+#   else
+#     # flash[:warning] = 'Oops!'
+#     redirect '/' #signup'
+#   end
+# end
+
+# post "/new_post" do
+#   @new_post = Post.create(
+#     post_title: params[:post_title],
+#     post_content: params[:post_content]
+#   )
+#   @new_post.save
+#   redirect "/posts/#{@new_post.id}" # or /dashboard?
+# end
+
+
+#dn logout TEST THIS
 # get "/sign_up" do
 #   erb :dashboard
 # end
+
 post "/sign_up" do
   @user = User.create(
     first_name: params[:first_name],
@@ -40,25 +82,21 @@ post "/sign_up" do
   redirect "/dashboard"
 end
 
-### Post routes ###
+#dn NEEDED?
+get "/log_in" do
 
-get "/new_post" do
-  erb :new_post
+  erb :index
 end
 
-post "/new_post" do
-  @new_post = Post.create(
-    post_title: params[:post_title],
-    post_content: params[:post_content]
-  )
-  @new_post.save
-  redirect "/posts/#{@new_post.id}"
-end
+# post "/log_out" do
+#   session['user_id'] = nil
+#   puts session
+#   redirect "/"
+# end
 
-
-#dn logout TEST THIS
-post "/log_out" do
-  session['user_id'] = nil
-  puts session
-  redirect "/"
-end
+#dn DELETE account
+# delete "/profile" do
+#   User.find(session[:user_id]).destroy
+#   session[:user_id] = nil
+#   redirect '/'
+# end 
